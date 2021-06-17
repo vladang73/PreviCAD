fa5017e176a3d80e7c48b5f1131b07898929a79f
Atta <atta.hameeed@gmail.com>
2021-06-17 05:18:31 +0500

    - Auto Refresh when there is a change, due to approval - File download option in new document section

--- Previgesst/Controllers/FileController.cs
+++ Previgesst/Controllers/FileController.cs
@@ -19,15 +19,16 @@ namespace Previgesst.Controllers
         private UtilisateurService utilisateurService;
         private DocumentClientRepository documentClientRepository;
         private EmployeRegistreService employeRegistreService;
+        private DocumentFicheRepository documentFicheRepository;
 
-        public FileController(DocumentRepository documentRepository, UtilisateurService utilisateurService,
+        public FileController(DocumentRepository documentRepository, UtilisateurService utilisateurService, DocumentFicheRepository documentFicheRepository,
             DocumentClientRepository documentClientRepository, EmployeRegistreService employeRegistreService)
         {
             this.utilisateurService = utilisateurService;
             this.documentRepository = documentRepository;
             this.documentClientRepository = documentClientRepository;
             this.employeRegistreService = employeRegistreService;
-
+            this.documentFicheRepository = documentFicheRepository;
         }
 
 
@@ -111,6 +112,54 @@ namespace Previgesst.Controllers
             var v = new AjoutFichierViewModel();
             v.TypeFichier = "Analyse de risques";
             return View(v);
+        }
+
+        public ActionResult DownloadFileFiche(int ID)
+        {
+            var ClientFromSession = utilisateurService.GetSession();
+
+            var empRegistre = employeRegistreService.getEmployeRegistre();
+
+
+
+            if (!Request.IsAuthenticated && ClientFromSession == null && empRegistre.ClientId == 0)
+            {
+                utilisateurService.LogOff();
+                this.Response.Redirect("~/ClientLogin/Index");
+            }
+
+
+
+            var file = documentFicheRepository.Get(ID);
+
+            /*
+            TODO: Not sure about permissions here
+
+
+            if (empRegistre.ClientId != 0)
+            {
+                if (file.ClientId != empRegistre.ClientId)
+                {
+                    employeRegistreService.LogOff();
+                    this.Response.Redirect("~/ClientLogin/Index");
+                }
+            }
+
+            if (ClientFromSession != null)
+            {
+                if (file.ClientId != ClientFromSession.ClientId)
+                {
+                    employeRegistreService.LogOff();
+                    this.Response.Redirect("~/ClientLogin/Index");
+                }
+            }
+
+            */
+
+            var strFilePath = Server.MapPath("~/FicheDocFiles/" + ID + "/" + file.FileName);
+            var docType = getDocType(strFilePath);
+            byte[] filePath = System.IO.File.ReadAllBytes(strFilePath);
+            return File(filePath, docType, Path.GetFileName(strFilePath));
         }
 
     }
