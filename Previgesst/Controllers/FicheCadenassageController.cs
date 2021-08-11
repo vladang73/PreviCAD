3a9e3618f05b55100326ecd6e0c6b985753c141f
Atta <atta.hameeed@gmail.com>
2021-08-12 00:19:35 +0500

    Paging and filtering in QR code print popup

--- Previgesst/Controllers/FicheCadenassageController.cs
+++ Previgesst/Controllers/FicheCadenassageController.cs
@@ -1207,7 +1207,7 @@ namespace Previgesst.Controllers
         //    var equipments = equipementService.GetEquipementsWithQR(id);
         //    return View(equipments);
         //}
-        
+
         public ActionResult PrintQR(int id, string opt, string ori, string equ)
         {
             var equipments = equipementService.GetEquipementsWithQR(id, equ);
@@ -1218,6 +1218,12 @@ namespace Previgesst.Controllers
         {
             var equipments = equipementService.GetEquipementsWithQR(id);
             return View(equipments);
+        }
+
+        public JsonResult OnlyWithQRData(int id)
+        {
+            var equipments = equipementService.GetEquipementsWithQR(id);
+            return Json(equipments, JsonRequestBehavior.AllowGet);
         }
     }
 }
